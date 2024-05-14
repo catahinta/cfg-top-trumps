568d400c505f3b995419a5701cd574315cb4c607
Kata Nagy <nagykata98@gmail.com>
2024-05-14 20:09:49 +0100

    improve ui

--- project.py
+++ project.py
@@ -1,15 +1,17 @@
 # This is the project file for CFG python course
 import random
 import requests
+import time
 
 players_id = random.randrange(1,151)
 computers_id = random.randrange(1,151)
 url = "https://pokeapi.co/api/v2/pokemon/"
 
-print("Hello and welcome to this Pokémon game!")
+print("Hello and welcome to this Pokémon game!\n")
 print("Getting you a random pokemon card")
 print("------------")
 print("------------")
+time.sleep(1)
 
 def get_pokemon(id):
     response = requests.get(url + str(id))
@@ -25,10 +27,11 @@ def get_pokemon(id):
 players_pokemon = get_pokemon(players_id)
 computers_pokemon = get_pokemon(computers_id)
 
-print("Your Pokémon is {}".format(players_pokemon["name"]))
-print("It's id is {}, it's height is {} and it's weight is {}".format(players_pokemon["id"], players_pokemon["height"], players_pokemon["weight"]))
+print("Your Pokémon is {}\n".format(players_pokemon["name"]))
+print("It's id is {}, it's height is {} and it's weight is {}\n".format(players_pokemon["id"], players_pokemon["height"], players_pokemon["weight"]))
+time.sleep(1)
 
-stat_choice = input("Which stat would you like to use?")
+stat_choice = input("Which stat would you like to use?\n")
 
 def compare_stats(stat):
     if players_pokemon[stat] > computers_pokemon[stat]:
@@ -41,8 +44,14 @@ def compare_stats(stat):
 game_result = compare_stats(stat_choice)
 
 print("You chose {}".format(stat_choice))
+print("------------")
+time.sleep(0.5)
 print("Your Pokémon's {} is {}".format(stat_choice, str(players_pokemon[stat_choice])))
+print("------------")
+time.sleep(1)
 print("The computer's Pokémon's {} is {}".format(stat_choice, str(computers_pokemon[stat_choice])))
+print("------------")
+time.sleep(1)
 
 if game_result == "player":
     print("YOU WIN!")
